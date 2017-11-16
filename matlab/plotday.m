794f0b56661241e739736a90b6defdb735b0666b
Carsten Wulff <carsten@wulff.no>
2017-11-16 22:56:30 +0100

    Added scripts to get pidata

--- matlab/plotday.m
+++ matlab/plotday.m
@@ -20,27 +20,67 @@
 function data=plotday(file);
 
 data = dlmread(file,";");
-avg1 = filter(ones(150,1)/150,1,data(:,2)); % 10 min average
 
-avg2 = filter(ones(900,1)/900,1,data(:,2)); % 1 hour average
+
+
+%avg1 = filter(ones(150,1)/150,1,data(:,2)); % 10 min average
+
+%avg2 = filter(ones(900,1)/900,1,data(:,2)); % 1 hour average
 
 x = (data(:,1) - data(1,1))/(60*60); %Hours
 
 setenv("GNUTERM","X11")
 figure1 = figure(1);
-date = ctime(data(1,1));
+
+
+%- Make average per hour
+hour_mean = zeros(1,24);
+for i=1:length(data(:,2))
+  time = localtime(data(i,1));
+  val = data(i,2);
+  hour_mean(time.hour+1) = (hour_mean(time.hour+1) + val)/2;
+end
+
+%- Make average per hour
+half_hour_mean = zeros(1,48);
+for i=1:length(data(:,2))
+  time = localtime(data(i,1));
+  val = data(i,2);
+  if(time.min >= 30) 
+	half_hour_mean(time.hour*2+2) = (half_hour_mean(time.hour*2+2) + val)/2;
+  else
+	half_hour_mean(time.hour*2+1) = (half_hour_mean(time.hour*2+1) + val)/2;
+  end
+end
+
+hour_x = (1:1:24);
+half_hour_x = (0.5:0.5:24);
+
+
+
 date = strftime ('%Y-%m-%d', localtime (data(1,1)))
 
 mm = mean(data(:,2))
 
+
+
+subplot(2,1,1);
 hold on;
 plot(x,data(:,2)/1000,'Color',[0.9 0.9 0.9]);
-plot(x,avg1/1000,'b');
-plot(x,avg2/1000,'r');
+%plot(x,avg1/1000,'b');
+%plot(x,avg2/1000,'r');
+stairs(half_hour_x,half_hour_mean/1000,'b');
+hold off;
 xlabel('Time [h]');
 ylabel('Power [kw]');
-title([date," Average=",sprintf('%.2f',mm/1000)," kW"]);
+title([date," Average=",sprintf('%.2f',mm/1000)," kW"," Total=",sprintf('%.2f',mm/1000*24)," kWh"]);
 axis([0 24 0 15])
+set(gca,"xtick",(1:1:24))
+adorne
+subplot(2,1,2),stairs(hour_x,hour_mean/1000,'r');
+xlabel('Time [h]');
+ylabel('Power [kw]');
+set(gca,"xtick",(1:1:24))
+axis([0 24 0 max(hour_mean/1000)])
 adorne
 saveas(figure1,[file,".pdf"]);
-hold off;
